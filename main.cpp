7db5548e41c79a750cd7ce312b1b815b5e81604a
rudraa19 <rudra19206@gmail.com>
2025-08-16 16:42:28 +0530

    chore: implement setup and display_grid

--- main.cpp
+++ main.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
 
 using namespace std;
 
+const int GRID_SIZE = 100;
+
+vector<vector<int>> grid;
+
+void setup();
+void display_grid();
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    setup();
+    display_grid();
     return 0;
+}
+
+void setup()
+{
+    srand(time(NULL));
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        vector<int> temp;
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            temp.push_back(rand() % 2);
+        }
+        grid.push_back(temp);
+    }
+}
+
+void display_grid()
+{
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
