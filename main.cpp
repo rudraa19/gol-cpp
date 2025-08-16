4346686fd7e538cf9ee902dec2180041830fb314
rudraa19 <rudra19206@gmail.com>
2025-08-16 16:55:01 +0530

    chore: implement update_grid functionality

--- main.cpp
+++ main.cpp
@@ -10,11 +10,17 @@ vector<vector<int>> grid;
 
 void setup();
 void display_grid();
+void update_grid();
+int count_neighbour(int x, int y);
 
 int main()
 {
     setup();
-    display_grid();
+    while (true)
+    {
+        display_grid();
+        update_grid();
+    }
     return 0;
 }
 
@@ -42,4 +48,34 @@ void display_grid()
         }
         cout << endl;
     }
+}
+
+void update_grid()
+{
+    vector<vector<int>> next_state = grid;
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            int n = count_neighbour(i, j);
+            if (grid[i][j] == 0 && n == 3)
+                next_state[i][j] = 1;
+            if (grid[i][j] == 1 && (n > 3 || n < 2))
+                next_state[i][j] = 0;
+        }
+    }
+    grid = next_state;
+}
+
+int count_neighbour(int x, int y)
+{
+    int count = 0;
+    for (int i = -1; i < 3; i++)
+    {
+        for (int j = -1; j < 3; j++)
+        {
+            count += grid[(i + x + GRID_SIZE) % GRID_SIZE][(j + y + GRID_SIZE) % GRID_SIZE];
+        }
+    }
+    return count - grid[x][y];
 }
